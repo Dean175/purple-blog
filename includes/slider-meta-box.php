10272323efeca8be0d61abd44bc3891cf96b082b
Dean175 <daredvl175@outlook.com>
2016-11-19 20:39:20 +0400

    added new js file slider-meta.js

--- includes/slider-meta-box.php
+++ includes/slider-meta-box.php
@@ -20,7 +20,8 @@ add_action( 'add_meta_boxes', 'pb_add_slider_meta_box' );
  * Add slider-meta-box script
  */
 function pb_slider_register_admin_script() {
-	wp_enqueue_script( 'pb_slider_meta', get_template_directory_uri() . '/js/slider-meta.js', true );
+	wp_enqueue_media();
+	wp_enqueue_script( 'pb_slider_meta', get_template_directory_uri() . '/js/slider-meta.js', array( 'jquery', 'media-upload' ), '1.12.4', true);
 }
 add_action( 'admin_enqueue_scripts', 'pb_slider_register_admin_script' );
 
--- js/slider-meta.js
+++ js/slider-meta.js
@@ -1,17 +1,30 @@
+
 var addButton = document.getElementById( 'image-upload-button' );
 var deleteButton = document.getElementById( 'image-delete-button' );
 var img = document.getElementById( 'image-tag' );
 var hidden = document.getElementById( 'img-hidden-field' );
+
+// Create a new media frame
 var customUploader = wp.media({
 	title: 'Select an Image',
 	button: {
 		text: 'Use this Image'
 	},
-	multiple: false
+	multiple: false // Set to true to allow multiple files to be selected
 });
 
 addButton.addEventListener( 'click', function() {
+	/* if customUploader exist */
 	if ( customUploader ) {
+		/* open it */
 		customUploader.open();
 	}
+} );
+
+// When an image is selected in the media frame
+customUploader.on( 'select', function(){
+
+	// Get media attachment details from the frame state
+	var attachment = customUploader.state().get('selection').first().toJSON();
+	img.set
 } );
